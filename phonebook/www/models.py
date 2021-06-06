8877ccf3edf8200527565fde03ad28900e4488ed
Marlen Alisherov <marlen.alisherov_2022@UCA.corp>
2021-06-06 17:17:15 +0600

    models done

--- phonebook/www/models.py
+++ phonebook/www/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    first_name = models.CharField(max_length= 100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
