a75f7ef64a67eda2027cd5540dc0ff01d8ab02b4
Atai Cholponkulov <atai.cholponkulov_2021@UCA.corp>
2021-06-06 17:17:44 +0600

    urls, views

---
+++ phonebook/www/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('main/', views.main, name='main'),
+    path('new_contact/', views.new_contact, name='new_contact'),
+    path('<int:contact_id>', views.show_contact, name="contact"),
+    path('update/', views.update_contact, name="update_contact"),
+]
+
+
