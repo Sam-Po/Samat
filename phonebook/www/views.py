bd77bcc3b7828df9c3f433113f75d65c74b848fd
Marlen Alisherov <marlen.alisherov_2022@UCA.corp>
2021-06-06 17:20:50 +0600

    migration created

--- phonebook/www/views.py
+++ phonebook/www/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect, HttpResponse
+# from django.urls import reverse
 
-def main(request):
-    contacts = Record.objects.all()
-    context = {
-        'contacts': contacts,
-    }
-    return render(request, '', context)
+# def main(request):
+#     contacts = Record.objects.all()
+#     context = {
+#         'contacts': contacts,
+#     }
+#     return render(request, '', context)
 
-def
